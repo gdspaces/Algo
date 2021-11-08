9c138ca10c5e9abe0a82029b2848fac37822e61e
Gourab <43839450+gdspaces@users.noreply.github.com>
2021-11-08 19:28:19 +0530

    Create Solve.py

---
+++ Solve.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'commonPrefix' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts STRING_ARRAY inputs as parameter.
+#
+
+def commonPrefix(inputs):
+    results=[] 
+    for str in inputs:
+        result = 0
+        all_suffix = [i.start() for i in re.finditer(str[:1], str)]
+        for i in all_suffix:
+            result = result+ len(os.path.commonprefix([str,str[i:]]))
+        results.append(result)            
+    return results 
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    
+    inputs_count = int(input().strip())
+    inputs = []
+    
+    for in range (inputs_count):
+        inputs_item = input()
+        inputs.append(inputs_item)
+    
+    result = commonPrefix(inputs)
+    fptr.write('n'.join(map (str, result)))
+    fptr.write('n')
+    fptr.close()
